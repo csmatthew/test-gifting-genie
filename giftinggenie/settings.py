b298d0361c0b0873a053752f4220f23511e4549c
csmatthew <christopher.s.matthew@gmail.com>
2025-02-02 08:59:21 +0000

    Initial commit

--- giftinggenie/settings.py
+++ giftinggenie/settings.py
@@ -24,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-'
-'66bpbclh%ie$on*igkdo_3_5wvi=ni4!zltc*(8&2#o#q86q71'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
