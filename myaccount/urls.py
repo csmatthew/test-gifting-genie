e6a6a5c7210278d19e7812fe2b59d136de2f2fe7
unknown <jtoconnor75@gmail.com>
2025-02-02 14:51:31 +0000

    users add edit view wishlists, no link from my account profile yet, append '/wishlist'

--- myaccount/urls.py
+++ myaccount/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import profile_view
+from .views import profile_view, user_wishlist_view
 
 urlpatterns = [
     path('', profile_view, name='myaccount_home'),
+    path('wishlist/', user_wishlist_view, name='myaccount_wishlist'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import WishlistItemForm
 from .models import WishlistItem
 
+
+@login_required
 def add_wishlist_item(request):
     if request.method == 'POST':
         form = WishlistItemForm(request.POST)
@@ -16,11 +19,14 @@ def add_wishlist_item(request):
         form = WishlistItemForm()
     return render(request, 'wishlist/add_wishlist_item.html', {'form': form})
 
+
+@login_required
 def wishlist_view(request):
     wishlist_items = WishlistItem.objects.filter(user=request.user)
     return render(request, 'wishlist/wishlist.html', {'wishlist_items': wishlist_items})
 
 
+@login_required
 def edit_wishlist_item(request, item_id):
     wishlist_item = get_object_or_404(WishlistItem, id=item_id, user=request.user)
     if request.method == 'POST':
@@ -33,6 +39,8 @@ def edit_wishlist_item(request, item_id):
         form = WishlistItemForm(instance=wishlist_item)
     return render(request, 'wishlist/edit_wishlist_item.html', {'form': form})
 
+
+@login_required
 def delete_wishlist_item(request, item_id):
     wishlist_item = get_object_or_404(WishlistItem, id=item_id, user=request.user)
     if request.method == 'POST':
