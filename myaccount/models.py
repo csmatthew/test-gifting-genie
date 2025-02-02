7ee942e026760884066bad789f6c6aa7e141bd89
csmatthew <christopher.s.matthew@gmail.com>
2025-02-02 11:23:16 +0000

    User name updates on profile.html. Link added for home, and small thumbnail next to user id on navbar

--- myaccount/models.py
+++ myaccount/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class MyAccount(models.Model):
-    user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
-    profile_image = models.FileField(upload_to='profile_images/', blank=True, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_image = models.ImageField(upload_to='profile_images', blank=True, null=True)
     title = models.CharField(max_length=100)
     content = models.TextField()
     updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.title
+        return self.user.username
